4e09f7f5b0f2eecb7255513a809215cd036ccd4b
willcattoneeca <will.catton@eeca.govt.nz>
2024-10-22 16:08:18 +1300

    Introduce test for space heating answers

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,9 +7,8 @@ python -m pytest --verbose
 """
 
 from fastapi.testclient import TestClient
-from pytest import approx, raises
+from pytest import raises
 
-from app.constants import DAYS_IN_YEAR
 from app.main import app
 from app.models.energy_plans import HouseholdEnergyPlan
 from app.models.user_answers import (
@@ -107,82 +106,6 @@ def test_create_household_energy_profile_to_cost():
     household_energy_use = estimate_usage_from_profile(household_profile)
     total_energy_costs = my_plan.calculate_cost(household_energy_use)
     assert sum(total_energy_costs) > 0
-
-
-def test_cooking_energy_usage():
-    """
-    Test the energy usage pattern for cooking.
-    """
-    your_home = get_default_your_home_answers()
-    cooktop = get_default_cooktop_answers()
-
-    # Modeled energy use in kWh for each cooktop type. This is based
-    # on a linearized energy use model that preserves the average
-    # household energy use for cooking. (See 'Cooking' sheet of
-    # supporting workbook.)
-    expected_energy_use = {
-        "Electric induction": [159, 239, 319, 398, 478, 558],
-        "Piped gas": [412, 618, 824, 1030, 1236, 1442],
-        "Bottled gas": [412, 618, 824, 1030, 1236, 1442, 1648],
-        "Electric (coil or ceramic)": [176, 264, 352, 440, 528, 617],
-    }
-
-    # Expected field values based on cooktop type
-    expected_values = {
-        "Electric induction": {
-            "elx_connection_days": DAYS_IN_YEAR,
-            "flexible_kwh": 0,
-            "natural_gas_kwh": 0,
-            "lpg_kwh": 0,
-            "natural_gas_connection_days": 0,
-            "lpg_tanks_rental_days": 0,
-        },
-        "Piped gas": {
-            "elx_connection_days": 0,
-            "day_kwh": 0,
-            "flexible_kwh": 0,
-            "lpg_kwh": 0,
-            "natural_gas_connection_days": DAYS_IN_YEAR,
-            "lpg_tanks_rental_days": 0,
-        },
-        "Bottled gas": {
-            "elx_connection_days": 0,
-            "day_kwh": 0,
-            "flexible_kwh": 0,
-            "natural_gas_connection_days": 0,
-            "lpg_tanks_rental_days": DAYS_IN_YEAR,
-        },
-        "Electric (coil or ceramic)": {
-            "elx_connection_days": DAYS_IN_YEAR,
-            "flexible_kwh": 0,
-            "natural_gas_kwh": 0,
-            "lpg_kwh": 0,
-            "natural_gas_connection_days": 0,
-            "lpg_tanks_rental_days": 0,
-        },
-    }
-
-    for cooktop_type, energy_use_values in expected_energy_use.items():
-        cooktop.cooktop = cooktop_type
-        for i, expected_kwh in enumerate(energy_use_values):
-            your_home.people_in_house = i + 1
-            cooktop_energy_use = cooktop.energy_usage_pattern(your_home)
-
-            # Assertions for expected energy usage (day_kwh, lpg_kwh, natural_gas_kwh)
-            if cooktop_type in ["Electric induction", "Electric (coil or ceramic)"]:
-                assert cooktop_energy_use.day_kwh == approx(expected_kwh, rel=1e-2)
-            elif cooktop_type == "Piped gas":
-                assert cooktop_energy_use.natural_gas_kwh == approx(
-                    expected_kwh, rel=1e-2
-                )
-            elif cooktop_type == "Bottled gas":
-                assert cooktop_energy_use.lpg_kwh == approx(expected_kwh, rel=1e-2)
-
-            # General assertions based on the cooktop type
-            for field, expected_value in expected_values[cooktop_type].items():
-                assert (
-                    getattr(cooktop_energy_use, field) == expected_value
-                ), f"{field} failed for {cooktop_type}"
 
 
 def test_create_options():
