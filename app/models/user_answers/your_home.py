3d198858ede1469463a58bbe81c724237cbde05c
willcattoneeca <will.catton@eeca.govt.nz>
2024-11-21 17:19:35 +1300

    Appropriate validation response if postcode is in EXCLUDE_POSTCODES

--- app/models/user_answers/your_home.py
+++ app/models/user_answers/your_home.py
@@ -5,9 +5,13 @@ Class for storing user answers on geography, household size and gas disconnectio
 
 from pydantic import BaseModel, conint, constr, model_validator
 
+from app.constants import EXCLUDE_POSTCODES
 from app.services.get_climate_zone import postcode_dict
 
 known_postcodes = set(postcode_dict.keys())
+exclude_postcodes = [
+    postcode for sublist in EXCLUDE_POSTCODES.values() for postcode in sublist
+]
 
 
 class YourHomeAnswers(BaseModel):
@@ -23,11 +27,15 @@ class YourHomeAnswers(BaseModel):
     def check_postcode(cls, model):
         """
         Validate that the postcode is recognized (a key from the
-        climate zone lookup)
+        climate zone lookup) and accepted (not in the exclude list).
 
         Raises:
             ValueError: If the postcode is not in the known list.
         """
+        if f"{model.postcode}" in exclude_postcodes:
+            raise ValueError(
+                f"Postcode '{model.postcode}' is not covered by this tool."
+            )
         if f"{model.postcode}" not in known_postcodes:
             raise ValueError(
                 f"Invalid postcode '{model.postcode}'. Please provide a valid postcode."
